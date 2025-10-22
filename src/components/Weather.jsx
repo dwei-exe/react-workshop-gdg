34d26b442aa40292b885e10bd871de7c46850a12
Daniel Wei <daniel.wei2006@hotmail.com>
2025-10-22 03:43:57 -0400

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Weather.css';
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png"
+import cloud_icon from "../assets/cloud.png"
+import rain_icon from "../assets/rain.png"
+import snow_icon from "../assets/snow.png"
+import wind_icon from "../assets/wind.png"
+import drizzle_icon from "../assets/drizzle.png"
+import humidity from "../assets/humidity.png"
+
+const Weather = () => {
+    const search = async () => {
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            console.log(data);
+
+        } catch (error) {
+
+        }
+    }
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt=""/>
+      </div>
+      <img src ={clear_icon} alt ="" className='weather-icon'/>
+      <p className='temperature'>16°C</p>
+      <p className='location'>London</p>
+    </div>
+  );
+};
+
+export default Weather;
