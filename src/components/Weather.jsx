7c1a14984116cf376d0d2ee8659bb57b0462eeb6
Daniel Wei <daniel.wei2006@hotmail.com>
2025-10-22 13:09:47 -0400

    API requests integration

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import './Weather.css';
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png"
@@ -10,19 +10,30 @@ import drizzle_icon from "../assets/drizzle.png"
 import humidity from "../assets/humidity.png"
 
 const Weather = () => {
-    const search = async () => {
+
+  const [weatherData, setWeatherData] = useState(false);
+  
+  const search = async (city) => {
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
             const response = await fetch(url);
             const data = await response.json();
 
             console.log(data);
 
-        } catch (error) {
+            setWeatherData({
+              temperature: data.main.temp,
 
+            });
+
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
         }
     }
+    useEffect(() => {
+      search("London");
+    }, []);
 
   return (
     <div className="weather">
